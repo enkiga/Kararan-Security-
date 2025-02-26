05c02ea67438857d91522245a517e38b1ce973ab
Enoch Garoli <eenkiga@gmail.com>
2025-02-26 22:33:26 +0300

    Routing Complete

--- app.js
+++ app.js
@@ -1,36 +1,13 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const app = express();
+const path = require("path");
 
-var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
+// Set EJS as template engine
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
 
 // Custom middleware for working hours
 app.use((req, res, next) => {
@@ -38,7 +15,7 @@ app.use((req, res, next) => {
   const day = now.getDay();
   const hour = now.getHours();
 
-  if (day >= 1 && day <= 5 && hour >= 9 && hour < 17) {
+  if (day >= 1 && day <= 5 && hour >= 9 && hour < 23) {
     next();
   } else {
     res.send(
